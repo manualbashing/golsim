ff84fffd2f9c1593057a56fbd2bcc054815df053
Manuel Batsching <flammschild@gmail.com>
2018-11-28 21:02:14 +0100

    Rewrite as module

---
+++ classes/Field.ps1
@@ -0,0 +1,82 @@
+class Field 
+{
+    [bool]$State
+    [bool]$PredictedNextState
+    [Field[]]$Neighbor
+    
+    Field([bool]$State)
+    {
+        $this.State = $State
+        $this.PredictedNextState = $false
+    }
+
+    [Field[]]GetNeighbor()
+    {
+        return $this.Neighbor
+    }
+    
+    [void]AddNeighbor([Field]$Field) 
+    {
+        if ($Field -notin $this.GetNeighbor()) 
+        {
+            $this.Neighbor += $Field    
+        }
+    }
+
+    [void]RegisterNeighbor([Field]$Field)
+    {
+        if ($null -ne $Field) 
+        {
+            $Field.AddNeighbor($this)
+            $this.AddNeighbor($Field)
+        }
+    }
+
+    [int]GetAliveNeighborSum() 
+    {
+        $sum = $this.GetNeighbor() | 
+            Measure-Object -Property State -Sum |
+            Select-Object -ExpandProperty Sum
+        return $sum
+    }
+
+    [void]UpdatePredictedNextState()
+    {
+        <#
+            Rules for new state:
+
+            - If a cell has less than two '1' neighbors, that cell becomes '0'
+            - If a cell has exactly two '1' neighbors, that cell does not change
+            - If a cell has exactly three '1' neighbors, that cell becomes '1'
+            - If a cell has more than three neighbors, that cell becomes '0'
+        #>
+
+        switch ($this.GetAliveNeighborSum()) 
+        {
+            {$_ -lt 2 -or $_ -gt 3} { $this.PredictedNextState = $false }
+            2 { $this.PredictedNextState = $this.State }
+            3 { $this.PredictedNextState = $true }
+            Default { }
+        }
+    }
+
+    [void]NextState()
+    {
+        $this.State = $this.PredictedNextState
+    }
+
+    [void]SetState([bool]$State)
+    {
+        $this.State = $State
+    }
+
+    [string]ToString() 
+    {
+        $stateString = ' '
+        if ($this.State) 
+        {
+            $stateString = 'X'    
+        }
+        return $stateString
+    }
+}
---
+++ classes/Game.ps1
@@ -0,0 +1,44 @@
+class Game 
+{
+    [Grid]$Grid
+    [int]$Iteration
+    [int]$Delay
+    
+
+    Game ([int]$Height, [int]$Width, [int]$Iteration, [int]$Delay) 
+    {
+        $this.Grid = [Grid]::new($Height,$Width)
+        $this.Iteration = $Iteration
+        $this.Delay = $Delay
+    }
+
+    Game ([int]$Height, [int]$Width, [String]$RLE, [int]$Iteration, [int]$Delay) 
+    {
+        $this.Grid = [Grid]::new($Height,$Width,$RLE)
+        $this.Iteration = $Iteration
+        $this.Delay = $Delay
+    }
+
+    Game ([string]$Path, [int]$Iteration, [int]$Delay) 
+    {
+        $this.Grid = [Grid]::new($Path)
+        $this.Iteration = $Iteration
+        $this.Delay = $Delay
+    }
+
+    StartGame()
+    {
+        Clear-Host
+        Write-Host "Iteration: "
+        Write-Host $this.Grid.ToString()
+        
+        for ($i = 1; $i -le $this.Iteration; $i++) 
+        {
+            $this.Grid.NextState()
+            Clear-Host
+            Write-Host "Iteration: $i"
+            Write-Host $this.Grid.ToString()
+            Start-Sleep -Milliseconds $this.Delay
+        }
+    }
+}
